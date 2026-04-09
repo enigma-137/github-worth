1843aad37b4f9e19cb2b5b44e6d549205bc4e470
enigma-137 <onojaemmanuel953@gmail.com>
2026-04-09 05:54:53 +0100

    Added migration script to package.json

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../prisma/generated/client"
 
 interface CustomNodeJsGlobal extends Global {
   prisma: PrismaClient | undefined
